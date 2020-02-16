19712cc6ca766d728db4038b83b76c82ae28cd7e
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-16 19:40:29 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,6 +6,16 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        {:hero_friend_one => {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        }
+        :hero_friend_two => {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }}
       ]
    }, 
    :capulet => {
@@ -14,6 +24,16 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {:heroine_friend_one => {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        }
+        :heroine_friend_two => {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }}
       ]
    }
 }
